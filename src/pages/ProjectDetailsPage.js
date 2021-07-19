66a3438526877d80c4f20969defdacd6a44cfa76
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-19 13:11:31 +0200

    Example 3

--- src/pages/ProjectDetailsPage.js
+++ src/pages/ProjectDetailsPage.js
@@ -1,12 +1,29 @@
-import projectsData from './../projects-data.json';
+import projectsData from "./../projects-data.json";
+import { Link } from "react-router-dom";            // <== IMPORT
 
 function ProjectDetailsPage(props) {
-  console.log(props);
+  // Method .find() returns the first found matching element,
+  // or `null` if no matching element was found.
+  const foundProject = projectsData.find((projectObj) => {        //  <== ADD
+    return projectObj._id === props.match.params.projectId;
+  });
+
   return (
     <div>
       <h1>Project Details</h1>
+      {!foundProject && <h3>Project not found!</h3>} {/* <== ADD  */}
+
+      {/*  ADD 👇 */}
+      {foundProject && (
+        <>
+          <h2>{foundProject.name}</h2>
+          <h3>Used technologies: {foundProject.technologies}</h3>
+          <p>{foundProject.description}</p>
+          <Link to="/projects">Back</Link>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 export default ProjectDetailsPage;
